04a73fa071fd54d2d9ac1e09efbe4d5a59ff562e
Szczepan Micek <szczypiorpan123@gmail.com>
2024-01-17 16:55:57 +0100

    Feat/landing (#1)
    * feat(ts): Add utility type and enums for theme and navigation
    
    - Implement a new utility type in TypeScript for enhanced type checking.
    - Create an enum for managing application themes.
    - Develop a second enum for streamlined app navigation handling.
    
    * feat(theme): Implement app theming with 'next-themes' and theme toggle button
    
    - Utilize 'next-themes' for efficient theme management in the application.
    - Add a button component for toggling between different themes.
    
    * feat(design): Add shadcn/ui button
    
    * feat(page): Implement landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { ThemeSwitcher } from '@/components/ThemeSwitcher';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-primary'>
-
-    </main>
+    <>
+      <Header className='flex-row-reverse'>
+        <ThemeSwitcher />
+      </Header>
+      <div className='flex-1 container bg-background flex flex-col justify-center items-center px-6 py-4'>
+        <h1 className='text-3xl w-full sm:w-auto sm:flex-row md:text-5xl lg:text-6xl flex flex-col px-6 font-bold text-center mb-8'>
+          <span className='mr-auto px-4 rounded-xl py-4'>Welcome to</span>
+          <span className='whitespace-nowrap ml-auto bg-foreground text-background px-4 rounded-xl py-4'>
+            Spell-Check
+          </span>
+        </h1>
+        <p className='text-lg text-center text-accent-foreground mb-8'>
+          Your ultimate tool for checking and correcting spelling errors in your
+          text.
+        </p>
+        <div className='flex gap-4'>
+          <Button>Try It Now</Button>
+          <Button variant='secondary'>I have an account</Button>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
